2a40446646acaa4a3a57f1f391821d50c1afa234
Ermanno Righini <ermanno.righini@student.unife.it>
2018-07-04 17:46:05 +0200

    ajskdlfkjkaldsf

--- test2.sh
+++ test2.sh
@@ -1,5 +1,95 @@
 #!/usr/bin/env bash
 
+./horn -b -m="discrete" -t=16 -l=1 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=1 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=2 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=2 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=3 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=3 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=4 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=4 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=5 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=5 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=6 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=6 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=7 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=7 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=8 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=8 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
+./horn -b -m="discrete" -t=16 -l=9 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=4 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=5 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=6 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=7 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=8 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+./horn -b -m="discrete" -t=16 -l=9 -c=9 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
+
 ./horn -b -m="discrete" -t=16 -l=10 -c=1 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
 ./horn -b -m="discrete" -t=16 -l=10 -c=2 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
 ./horn -b -m="discrete" -t=16 -l=10 -c=3 --clause_len=3 --max_false_clauses=1 --stop --batch_size=5
